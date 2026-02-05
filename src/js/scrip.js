c39b073dbf2bbd1cf742f1fa954fe675c991b793
Taissa Rodrigues <taissavrp@gmail.com>
2026-02-05 13:28:05 -0300

    feat: funcao de validar email

---
+++ src/js/scrip.js
@@ -0,0 +1,17 @@
+const form = document.querySelector("#form")
+const nameInput = document.querySelector("#name")
+const lastNameinput = document.querySelector("#lastName")
+const emailInput = document.querySelector("#email")
+const passwordInput = document.querySelector("#password")
+const messageTextarea = document.querySelector("#message")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // verifica se o nome esta vazio.
+
+    if (nameInput.value === "") {
+        alert ("Por favor, preencha o seu nome");
+        return;
+    }
+});
